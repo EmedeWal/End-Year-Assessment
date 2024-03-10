63bbd0449926e2958b12d6c96d5854b0bd9f9170
EmedeWal <emewillemdewal@gmail.com>
2024-03-10 15:52:19 +0100

    Commit Imported Player Character
    Imported the player character and fixed attack, run, and idle animations

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,27 +1,48 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    [Header("References")]
+    [SerializeField] private Animator animator;
+
     [Header("Variables")]
     [SerializeField] private float moveSpeed;
     [SerializeField] private float rotationSpeed;
 
     private Vector2 move;
+    public bool canMove = true;
+
+    public float attackSpeed;
+    public float attackOffset;
+    public bool hasAttacked;
 
     private void Update()
     {
+        if (canMove)
         MovePlayer();
     }
 
-    // Handle Input
+    #region Input
+
     public void OnMove(InputAction.CallbackContext context)
     {
         move = context.ReadValue<Vector2>();
     }
 
-    // Move the player
-    public void MovePlayer()
+    public void OnAttack()
+    {
+        Attack();
+    }
+
+    #endregion
+
+
+
+    #region Functionality
+
+    private void MovePlayer()
     {
         Vector3 movement = new Vector3(move.x, 0f, move.y);
 
@@ -34,5 +55,34 @@ public class PlayerController : MonoBehaviour
 
         // This apparently works really well so let's remember transform.Translate
         transform.Translate(movement * moveSpeed * Time.deltaTime, Space.World);
+
+        // Check if the player is running or not, then set the correct animation
+        animator.SetFloat("Movement", movement.magnitude);
     }
+
+    private void Attack()
+    {
+        if (!hasAttacked)
+        {
+            hasAttacked = true;
+            canMove = false;
+            animator.SetTrigger("Attack");
+            StartCoroutine(ResetAttack());
+        }   
+    }
+
+    private IEnumerator ResetAttack()
+    {
+        // Wait for the attackSpeed of the player, before the player can attack again
+        yield return new WaitForSeconds(attackSpeed - attackOffset);
+        hasAttacked = false;
+
+        // Wait a little longer before making the player move again, so that animations do not trigger double and the player can "chain attack"
+        yield return new WaitForSeconds(attackOffset);
+        if (!hasAttacked) canMove = true;
+    }
+
+    #endregion
+
+
 }
